2ceb8f5379e593774faa53bde134ce786e77ea38
karolbelo <karolbello98@gmail.com>
2021-06-29 15:46:45 -0300

    js

--- ada/js/script.js
+++ ada/js/script.js
@@ -1,8 +1,14 @@
 
+documento.getElementById("botaoEnviar").addEventListener("click", validaformulario)
 
+function validaformulario(){
 
-if (document.getElementById("nome") && document.getElementById("email");) {
-    
-}else{
+    if (document.getElementById("nome").value != "" && 
+    document.getElementById("email").value != ""){
+        alert("Prontinho! você receberá as novidades por email")
+    }
+    else{
+        alert("Por favor, preencha os campos nome e email")
+    }
 
 }
